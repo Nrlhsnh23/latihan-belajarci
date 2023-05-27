a98ab27cd5736a9f7b6e616993e0df9a812e0590
Nrlhsnh23 <nurulhasnh3@gmail.com>
2023-05-27 19:08:53 +0700

    push code

---
+++ app/Views/layouts/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer ">
+    <div class="container ">
+        <span class="text-muted ">Mutiara Azzahrani Artama.</span>
+    </div>
+</footer>
---
+++ app/Views/layoutsV2/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Mutiara Azzahrani </span>
+                    </div>
+                </div>
+            </footer>
